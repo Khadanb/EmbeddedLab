7e7676c538877e0d7fc934d93e66ecb3acddd215
Brandon Khadan <khadanb@lastgateway.com>
2024-04-29 12:08:29 -0400

    update move

--- Project_hw/move.sh
+++ Project_hw/move.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 read -p "Enter Computer Number: " ComputerNumber
 
-mount /dev/mmcblk0p1 /mnt
-
 scp "bk2746@micro${ComputerNumber}.ee.columbia.edu:~/Projects/EmbeddedLab/Project_hw/output_files/soc_system.rbf" /mnt
 
 scp "bk2746@micro${ComputerNumber}.ee.columbia.edu:~/Projects/EmbeddedLab/soc_system.dtb" /mnt
