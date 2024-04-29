3ebf0f3cabce06ae88a8e968f77f4b8c2bdb1811
Brandon Khadan <khadanb@lastgateway.com>
2024-04-29 12:15:41 -0400

    fix script

--- Project_hw/move.sh
+++ Project_hw/move.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 read -p "Enter Computer Number: " ComputerNumber
 
+mount /dev/mmcblk0p1 /mnt
+
 scp "bk2746@micro${ComputerNumber}.ee.columbia.edu:~/Projects/EmbeddedLab/Project_hw/output_files/soc_system.rbf" /mnt
 
-scp "bk2746@micro${ComputerNumber}.ee.columbia.edu:~/Projects/EmbeddedLab/soc_system.dtb" /mnt
+scp "bk2746@micro${ComputerNumber}.ee.columbia.edu:~/Projects/EmbeddedLab/Project_hw/soc_system.dtb" /mnt
 
 echo "Transfer initiated for micro${ComputerNumber}."
