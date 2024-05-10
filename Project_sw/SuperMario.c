a3d70be01c02726bd4b1294ce84dffbe6b5d7aae
Brandon Khadan <khadanb@lastgateway.com>
2024-05-10 16:09:59 -0400

    update

--- Project_sw/SuperMario.c
+++ Project_sw/SuperMario.c
@@ -337,8 +337,11 @@ void process_mario_logic(Entity *mario, Game *game) {
 	}
 
 	// Update position
-	game->camera_pos += mario->motion.vx;
-	game->camera_velocity = mario->motion.vx;
+
+	if ((game->camera_pos + mario->motion.vx) > 0) {
+		game->camera_pos + mario->motion.vx : 0;
+		game->camera_velocity = mario->motion.vx;
+	}
 	mario->position.y += mario->motion.vy;
 	mario->position.x = game->camera_pos + 128;
 
