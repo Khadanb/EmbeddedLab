e1283e733f719b19dea57fa4318cabb1fe3ee2c5
Brandon Khadan <khadanb@lastgateway.com>
2024-05-11 18:13:18 -0400

    fix path

--- Project_hw/Bowser_display.sv
+++ Project_hw/Bowser_display.sv
@@ -120,7 +120,7 @@ module Bowser_display (
 	end
 		
 initial begin
-	$readmemh("/user/stud/fall22/hy2759/4840/pro_test/lab3-hw/on_chip_mem/Bowser.txt", mem);
+	$readmemh("/user/stud/fall21/bk2746/Projects/EmbeddedLab/Project_hw/on_chip_mem/Bowser.txt", mem);
 end
 
    
