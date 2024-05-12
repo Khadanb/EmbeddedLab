f15223250ffe961ba0d8dc00b90eb4f8f9abf40d
Brandon Khadan <khadanb@lastgateway.com>
2024-05-11 22:55:56 -0400

    moved audio mod to correct loc

--- Project_hw/audiomodule2.sv
+++ Project_hw/audiomodule2.sv
@@ -63,7 +63,7 @@ module I2S_Controller(
                         ready <= 1;
                     end
                 end
-            } else {
+            end else begin
                 clk_count <= clk_count + 1;
             end
         end
