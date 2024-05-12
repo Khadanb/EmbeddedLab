c72982c6026edbf5a2f0ef3f9f40290076933a7b
Brandon Khadan <khadanb@lastgateway.com>
2024-05-12 12:17:42 -0400

    update

--- Project_sw/game_animation.c
+++ Project_sw/game_animation.c
@@ -37,7 +37,7 @@ void animate_entity(Game *game, Entity *entity, int f_counter) {
 
 void animate_mario(Game *game, Entity *entity, int f_counter) {
     if (entity->state.active && entity->render.visible) {
-        const int frame_count = 10; // Update sprite every 10 frames
+        const int frame_count = 5; // Update sprite every 10 frames
         int rel_counter = f_counter % frame_count; // Calculate frame counter relative to the frame_count
 
         float dead_v = -4.7f;
@@ -51,7 +51,7 @@ void animate_mario(Game *game, Entity *entity, int f_counter) {
                     } else if (entity->motion.ax * entity->motion.vx < 0) {
                         entity->render.pattern_code = ANI_MARIO_S_SHUT;
                     } else {
-                        int ani_stage = (f_counter / 3) % 3; // Change to walk animation frames
+                        int ani_stage = (f_counter / 6) % 3; // Change to walk animation frames
                         entity->render.pattern_code = (ani_stage == 0) ? ANI_MARIO_S_WALK1 :
                                                      (ani_stage == 1) ? ANI_MARIO_S_WALK2 :
                                                      ANI_MARIO_S_WALK3;
